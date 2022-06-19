c9c7178ce960b9940f1f08d581d9b68d069717e6
Gajendra Gaud <gaud786gajendra@gmail.com>
2022-06-19 09:54:17 +0530

    PH-08 : Implements save method from service layer

--- src/main/java/com/PhoneBook/Service/ContactServiceImp.java
+++ src/main/java/com/PhoneBook/Service/ContactServiceImp.java
@@ -1,15 +1,27 @@
 package com.PhoneBook.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.PhoneBook.Model.Contact;
+import com.PhoneBook.Repository.ContactRepository;
 
 @Service
 public class ContactServiceImp implements ContactService{
 
+	@Autowired
+	private ContactRepository contactRepository;
+	
+	
 	@Override
 	public boolean saveContact(Contact contact) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub IllegalArgumentException
+		
+		Contact save = contactRepository.save(contact);
+		if(save !=null)
+		{
+			return true;
+		}
 		return false;
 	}
 
