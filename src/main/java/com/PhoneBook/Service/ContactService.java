62653907ad4838c0bbfdf26846a89cd00c243abf
Gajendra Gaud <gaud786gajendra@gmail.com>
2022-06-19 09:36:35 +0530

    PH-06 : Declare Save method in service interface

--- src/main/java/com/PhoneBook/Service/ContactService.java
+++ src/main/java/com/PhoneBook/Service/ContactService.java
@@ -1,8 +1,9 @@
 package com.PhoneBook.Service;
 
+import com.PhoneBook.Model.Contact;
+
 public interface ContactService {
 	
-	
-	
+	public abstract boolean saveContact(Contact contact);
 
 }
--- src/main/java/com/PhoneBook/Service/ContactServiceImp.java
+++ src/main/java/com/PhoneBook/Service/ContactServiceImp.java
@@ -2,7 +2,15 @@ package com.PhoneBook.Service;
 
 import org.springframework.stereotype.Service;
 
+import com.PhoneBook.Model.Contact;
+
 @Service
 public class ContactServiceImp implements ContactService{
+
+	@Override
+	public boolean saveContact(Contact contact) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
